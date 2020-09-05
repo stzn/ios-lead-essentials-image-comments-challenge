e53b841631d4d62265f573fc026b0b4a3c41808a
stzn <sztk1209@gmail.com>
2020-09-06 08:22:15 +0900

    replace ImageCommentsPresenter with generic Presenter

--- EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsLoadingViewModel.swift
+++
@@ -1,7 +0,0 @@
-//	
-// Copyright © 2020 Essential Developer. All rights reserved.
-//
-
-public struct ImageCommentsLoadingViewModel {
-    public let isLoading: Bool
-}
--- EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -8,88 +8,18 @@ import EssentialFeed
 class ImageCommentsPresenterTests: XCTestCase {
 
     func test_title_isLocalized() {
-        XCTAssertEqual(ImageCommentsPresenter.title, localized("COMMENTS_VIEW_TITLE"))
-    }
-
-    func test_init_doesNotSendMessagesToView() {
-        let (_, view) = makeSUT()
-
-        XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
-    }
-
-    func test_didStartLoadingImageComments_displaysNoErrorMessageAndStartsLoading() {
-        let (sut, view) = makeSUT()
-
-        sut.didStartLoadingImageComments()
-
-        XCTAssertEqual(view.messages, [
-            .display(errorMessage: .none),
-            .display(isLoading: true)
-        ])
-    }
-
-    func test_didFinishLoadingImageComments_displaysImageCommentsAndStopsLoading() {
-        let (sut, view) = makeSUT()
-        let comments = uniqueImageComments()
-
-        sut.didFinishLoadingImageComments(with: comments)
-
-        XCTAssertEqual(view.messages, [
-            .display(comments: comments),
-            .display(isLoading: false)
-        ])
-    }
-
-    func test_didFinishLoadingImageCommentsWithError_displaysLocalizedErrorMessageAndStopsLoading() {
-        let (sut, view) = makeSUT()
-
-        sut.didFinishLoadingImageComments(with: anyNSError())
-
-        XCTAssertEqual(view.messages, [
-            .display(errorMessage: localized("COMMENTS_VIEW_CONNECTION_ERROR")),
-            .display(isLoading: false)
-        ])
+        XCTAssertEqual(ImageCommentsPresenter.title, localized(ImageCommentsPresenter.titleKey))
     }
 
     // MARK: - Helpers
 
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: ImageCommentsPresenter, view: ViewSpy) {
-        let view = ViewSpy()
-        let sut = ImageCommentsPresenter(commentsView: view, loadingView: view, errorView: view)
-        trackForMemoryLeaks(view, file: file, line: line)
-        trackForMemoryLeaks(sut, file: file, line: line)
-        return (sut, view)
-    }
-
     private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
-        let table = "Comments"
+        let table = ImageCommentsPresenter.stringsFileName
         let bundle = Bundle(for: ImageCommentsPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
             XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
         }
         return value
-    }
-
-    private class ViewSpy: ImageCommentsView, ImageCommentsLoadingView, ImageCommentsErrorView {
-        enum Message: Hashable {
-            case display(errorMessage: String?)
-            case display(isLoading: Bool)
-            case display(comments: [ImageComment])
-        }
-
-        private(set) var messages = Set<Message>()
-
-        func display(_ viewModel: ImageCommentsViewModel) {
-            messages.insert(.display(comments: viewModel.comments))
-        }
-
-        func display(_ viewModel: ImageCommentsLoadingViewModel) {
-            messages.insert(.display(isLoading: viewModel.isLoading))
-        }
-
-        func display(_ viewModel: ImageCommentsErrorViewModel) {
-            messages.insert(.display(errorMessage: viewModel.message))
-        }
     }
 }
