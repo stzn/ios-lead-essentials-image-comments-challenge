8571000233a342d73944fbce8f8dec56017d2166
stzn <sztk1209@gmail.com>
2020-09-05 12:40:49 +0900

    move common test helper to SharedTestHelpers

--- EssentialFeed/EssentialFeedTests/Feed API/FeedImagesMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedImagesMapperTests.swift
@@ -64,9 +64,3 @@ class FeedImagesMapperTests: XCTestCase {
 		return try! JSONSerialization.data(withJSONObject: json)
 	}
 }
-
-extension HTTPURLResponse {
-    convenience init(statusCode: Int) {
-        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
-    }
-}
