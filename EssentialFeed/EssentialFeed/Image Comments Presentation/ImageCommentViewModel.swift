9170727771a8a316c81a4a625d4698d3e9901c21
stzn <sztk1209@gmail.com>
2020-09-09 09:49:41 +0900

    Add ImageCommentViewModel

---
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentViewModel.swift
@@ -0,0 +1,12 @@
+//	
+// Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct ImageCommentViewModel {
+    let id: UUID
+    let message: String
+    let createdAt: String
+    let username: String
+}
