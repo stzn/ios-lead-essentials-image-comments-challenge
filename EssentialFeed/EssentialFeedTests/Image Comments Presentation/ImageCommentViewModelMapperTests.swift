f33df1220bf1ea99c6e67c419f197e2c30dc47d9
stzn <sztk1209@gmail.com>
2020-09-11 08:10:47 +0900

    Implement ImageCommentPresenter

--- EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentViewModelMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentViewModelMapperTests.swift
@@ -3,10 +3,10 @@
 //
 
 import XCTest
-@testable import EssentialFeed
+import EssentialFeed
 
-class ImageCommentViewModelMapperTests: XCTestCase {
-    func test_map_imageCommentsToImageCommentViewModel() throws {
+class ImageCommentCreatedAtDateFormatterTests: XCTestCase {
+    func test_format_dateToString() {
         let now = Date()
 
         let moreThanOneHourAgo = now.adding(seconds: -TimeInterval(60*60*1 + 1))
@@ -14,31 +14,16 @@ class ImageCommentViewModelMapperTests: XCTestCase {
         let moreThanOneWeekAgo = now.adding(seconds: -TimeInterval(60*60*24*7 + 1))
         let moreThanOneMonthAgo = now.adding(seconds: -TimeInterval(60*60*24*31 + 1))
         let moreThanOneYearAgo = now.adding(seconds: -TimeInterval(60*60*24*31*12 + 1))
-        let samples: [(ImageComment, String)] = [
-            (makeComment(createdAt: moreThanOneHourAgo), "1 hour ago"),
-            (makeComment(createdAt: moreThanOneDayAgo), "1 day ago"),
-            (makeComment(createdAt: moreThanOneWeekAgo), "1 week ago"),
-            (makeComment(createdAt: moreThanOneMonthAgo), "1 month ago"),
-            (makeComment(createdAt: moreThanOneYearAgo), "1 year ago"),
+        let samples: [(Date, String)] = [
+            (moreThanOneHourAgo, "1 hour ago"),
+            (createdAt: moreThanOneDayAgo, "1 day ago"),
+            (createdAt: moreThanOneWeekAgo, "1 week ago"),
+            (createdAt: moreThanOneMonthAgo, "1 month ago"),
+            (createdAt: moreThanOneYearAgo, "1 year ago"),
         ]
 
-        try samples.forEach { (comment, expectedDateString) in
-            XCTAssertNoThrow(try ImageCommentViewModelMapper.map(comment))
-            let viewModel = try! ImageCommentViewModelMapper.map(comment)
-            XCTAssertEqual(viewModel.id, comment.id)
-            XCTAssertEqual(viewModel.message, comment.message)
-            XCTAssertEqual(viewModel.username, comment.username)
-            XCTAssertEqual(viewModel.createdAt, expectedDateString)
+        samples.forEach { (date, expectedDateString) in
+            XCTAssertEqual(expectedDateString, ImageCommentCreatedAtDateFormatter.format(from: date))
         }
-    }
-
-    // MARK: - Helpers
-
-    private func makeComment(
-        message: String = "any message",
-        createdAt: Date = Date(),
-        username: String = "any username"
-    ) -> ImageComment {
-        return ImageComment(id: UUID(), message: message, createdAt: createdAt, username: username)
     }
 }
