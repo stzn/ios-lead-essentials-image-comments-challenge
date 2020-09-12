d12fd746d28a4ec93d4d9a4d94f624025481e29a
stzn <sztk1209@gmail.com>
2020-09-12 13:05:13 +0900

    delete comment

--- EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -12,31 +12,3 @@ public final class ImageCommentsLocalizedString {
                                  comment: "Title for the feed view")
     }
 }
-
-//public protocol ImageCommentsView {
-//    func display(_ viewModel: ImageCommentsViewModel)
-//}
-
-//public final class ImageCommentsPresenter {
-//    private let commentsView: ImageCommentsView
-//    private let loadingView: LoadingView
-//    private let errorView: ErrorView
-//
-//    public init(commentView: ImageCommentsView, loadingView: LoadingView, errorView: ErrorView) {
-//        self.commentsView = commentView
-//        self.loadingView = loadingView
-//        self.errorView = errorView
-//    }
-//
-//    public static var title: String {
-//        return NSLocalizedString("VIEW_TITLE",
-//                                 tableName: "ImageComments",
-//                                 bundle: Bundle(for: ImageCommentsPresenter.self),
-//                                 comment: "Title for the feed view")
-//    }
-//
-//    public func didFinishLoadingImageComments(with comments: [ImageComment]) {
-//        commentsView.display(ImageCommentsViewModel(comments: comments))
-//        loadingView.display(LoadingViewModel(isLoading: false))
-//    }
-//}
