2dc70d5c18d25bbd88ba35d54f845bbc706676df
stzn <sztk1209@gmail.com>
2020-09-25 19:40:53 +0900

    delete unused test helper

--- EssentialFeed/EssentialFeedTests/Helpers/XCTestCase+LocalizedString.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/XCTestCase+LocalizedString.swift
@@ -44,19 +44,4 @@ extension XCTestCase {
             return acc.union(Set(keys))
         }
     }
-
-    func sharedLocalized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
-        let table = "Shared"
-        let bundle = Bundle(for: Presenter<Never, AnyView>.self)
-        let value = bundle.localizedString(forKey: key, value: nil, table: table)
-        if value == key {
-            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
-        }
-        return value
-    }
-
-    private class AnyView: ResourceView {
-        typealias Content = Never
-        func display(_ model: Never) {}
-    }
 }
