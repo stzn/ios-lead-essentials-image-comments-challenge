f6e62e69f35a29aef9836d060086acd512ede2ab
stzn <sztk1209@gmail.com>
2020-10-11 10:04:05 +0900

    fix syntax

--- EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -72,16 +72,14 @@ class FeedSnapshotTests: XCTestCase {
 
 private extension ListViewController {
     func display(_ stubs: [ImageStub]) {
-        let cells: [FeedImageCellController] = stubs.map { stub in
+        let cells: [CellController] = stubs.map { stub in
             let cellController = FeedImageCellController(
                 viewModel: stub.viewModel, delegate: stub, selection: { })
             stub.controller = cellController
-            return cellController
+            return CellController(id: UUID(), dataSoruce: cellController, delegate: cellController, prefetchDataSource: cellController)
         }
 
-        display(cells.map {
-                    CellController(id: UUID(), dataSoruce: $0, delegate: $0, prefetchDataSource: $0) }
-        )
+        display(cells)
     }
 }
 
