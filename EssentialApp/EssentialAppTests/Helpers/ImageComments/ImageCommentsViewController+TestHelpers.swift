6316a2af283338b33dff0aad8491ba3b0da2dc73
stzn <sztk1209@gmail.com>
2020-10-11 11:08:41 +0900

    fix section name

--- EssentialApp/EssentialAppTests/Helpers/ImageComments/ImageCommentsViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ImageComments/ImageCommentsViewController+TestHelpers.swift
@@ -20,7 +20,7 @@ extension ListViewController {
         let view = simulateImageCommentsImageViewVisible(at: row)
 
         let delegate = tableView.delegate
-        let index = IndexPath(row: row, section: feedImagesSection)
+        let index = IndexPath(row: row, section: imageCommentsSection)
         delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
 
         return view
@@ -28,7 +28,7 @@ extension ListViewController {
 
     func simulateImageCommentsImageViewNearVisible(at row: Int) {
         let ds = tableView.prefetchDataSource
-        let index = IndexPath(row: row, section: feedImagesSection)
+        let index = IndexPath(row: row, section: imageCommentsSection)
         ds?.tableView(tableView, prefetchRowsAt: [index])
     }
 
@@ -36,12 +36,12 @@ extension ListViewController {
         simulateImageCommentsImageViewNearVisible(at: row)
 
         let ds = tableView.prefetchDataSource
-        let index = IndexPath(row: row, section: feedImagesSection)
+        let index = IndexPath(row: row, section: imageCommentsSection)
         ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
     }
 
     func numberOfRenderedImageCommentsViews() -> Int {
-        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: feedImagesSection)
+        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: imageCommentsSection)
     }
 
     func imageCommentsView(at row: Int) -> UITableViewCell? {
@@ -49,11 +49,16 @@ extension ListViewController {
             return nil
         }
         let ds = tableView.dataSource
-        let index = IndexPath(row: row, section: feedImagesSection)
+        let index = IndexPath(row: row, section: imageCommentsSection)
         return ds?.tableView(tableView, cellForRowAt: index)
     }
 
-    private var feedImagesSection: Int {
+    func commentMessage(at row: Int) -> String? {
+        let cell = imageCommentsView(at: row) as! ImageCommentCell
+        return cell.messageLabel?.text
+    }
+
+    private var imageCommentsSection: Int {
         return 0
     }
 }
