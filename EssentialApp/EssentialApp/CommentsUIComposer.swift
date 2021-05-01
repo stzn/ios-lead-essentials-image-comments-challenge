e63022d5dbd8c3871413c8aa81ff3d64b76cb5f4
stzn <sztk1209@gmail.com>
2021-05-01 13:05:03 +0900

    duplicate FeedUIComposer as CommentsUIComposer

---
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -0,0 +1,44 @@
+//
+//  Copyright © 2019 Essential Developer. All rights reserved.
+//
+
+import UIKit
+import Combine
+import EssentialFeed
+import EssentialFeediOS
+
+public final class CommentsUIComposer {
+	private init() {}
+
+	private typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<[FeedImage], ImageCommentsViewAdapter>
+
+	public static func feedComposedWith(
+		feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>,
+		imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher,
+		selection: @escaping (FeedImage) -> Void = { _ in }
+	) -> ListViewController {
+		let presentationAdapter = FeedPresentationAdapter(loader: feedLoader)
+
+		let feedController = makeFeedViewController(title: FeedPresenter.title)
+		feedController.onRefresh = presentationAdapter.loadResource
+
+		presentationAdapter.presenter = LoadResourcePresenter(
+			resourceView: ImageCommentsViewAdapter(
+				controller: feedController,
+				imageLoader: imageLoader,
+				selection: selection),
+			loadingView: WeakRefVirtualProxy(feedController),
+			errorView: WeakRefVirtualProxy(feedController),
+			mapper: FeedPresenter.map)
+
+		return feedController
+	}
+
+	private static func makeFeedViewController(title: String) -> ListViewController {
+		let bundle = Bundle(for: ListViewController.self)
+		let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+		let feedController = storyboard.instantiateInitialViewController() as! ListViewController
+		feedController.title = title
+		return feedController
+	}
+}
---
+++ EssentialApp/EssentialApp/ImageCommentsViewAdapter.swift
@@ -0,0 +1,44 @@
+//
+//  Copyright © 2019 Essential Developer. All rights reserved.
+//
+
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+
+final class ImageCommentsViewAdapter: ResourceView {
+	private weak var controller: ListViewController?
+	private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
+	private let selection: (FeedImage) -> Void
+
+	private typealias ImageDataPresentationAdapter = LoadResourcePresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>
+
+	init(controller: ListViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher, selection: @escaping (FeedImage) -> Void) {
+		self.controller = controller
+		self.imageLoader = imageLoader
+		self.selection = selection
+	}
+
+	func display(_ viewModel: FeedViewModel) {
+		controller?.display(viewModel.feed.map { model in
+			let adapter = ImageDataPresentationAdapter(loader: { [imageLoader] in
+				imageLoader(model.url)
+			})
+
+			let view = FeedImageCellController(
+				viewModel: FeedImagePresenter.map(model),
+				delegate: adapter,
+				selection: { [selection] in
+					selection(model)
+				})
+
+			adapter.presenter = LoadResourcePresenter(
+				resourceView: WeakRefVirtualProxy(view),
+				loadingView: WeakRefVirtualProxy(view),
+				errorView: WeakRefVirtualProxy(view),
+				mapper: UIImage.tryMake)
+
+			return CellController(id: model, view)
+		})
+	}
+}
